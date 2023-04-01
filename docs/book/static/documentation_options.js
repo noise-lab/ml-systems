e775d99d83506e8ed3b1019025c5cb9f3b7c2408
Nick Feamster <feamster@uchicago.edu>
2023-03-31 19:34:37 -0500

    book update

--- docs/book/static/documentation_options.js
+++ docs/book/static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version 0.1-dev',
-    LANGUAGE: 'None',
+    VERSION: 'Version 0.1',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
